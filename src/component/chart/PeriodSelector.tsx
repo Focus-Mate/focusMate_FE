6b583593a480490475e0f5d1f2956aab3684a46b
김민경 <105103167+sparrowscout@users.noreply.github.com>
2022-12-26 20:49:16 +0900

    [implement] datepicker - daily tab

--- src/component/chart/PeriodSelector.tsx
+++ src/component/chart/PeriodSelector.tsx
@@ -1,49 +1,115 @@
 import styled from "styled-components";
 import { LeftArrowIcon, RightArrowIcon } from "../../style/icon/chartPage";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-datepicker/dist/react-datepicker-cssmodules.css";
+import { getTodayDate } from "../../util";
+import ko from "date-fns/locale/ko";
+import format from "date-fns/format";
+import { add, sub } from "date-fns";
 
 const PeriodSelector = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const [isToday, setIsToday] = useState(true);
+
+  const dateAdd = () => {
+    const result = add(startDate, { days: 1 });
+    setStartDate(result);
+    console.log(startDate);
+  };
+
+  const dateSub = () => {
+    const result = sub(startDate, { days: 1 });
+    setStartDate(result);
+    console.log(startDate);
+  };
+
+  const todayCheck = () => {
+    const today = getTodayDate();
+    const selectedDate = format(startDate, "MM/dd");
+    if (today === selectedDate) {
+      setIsToday(true);
+    } else {
+      setIsToday(false);
+    }
+  };
+  useEffect(() => {
+    todayCheck();
+  }, [startDate]);
   return (
-    <>
-      <PeriodSelectWrapper>
-        <PeriodSelectBtn>
-          <LeftArrowIcon />
-        </PeriodSelectBtn>
+    <PeriodSelectWrapper>
+      <PeriodSelectBtn onClick={dateSub}>
+        <LeftArrowIcon />
+      </PeriodSelectBtn>
 
-        <ReactDatePicker
-          selected={startDate}
-          onChange={(date: Date) => setStartDate(date)}
-        />
+      <PeriodPicker
+        selected={startDate}
+        onChange={(date: Date) => setStartDate(date)}
+        dateFormat="MM/dd (eee)"
+        locale={ko}
+        showPopperArrow={false}
+      />
 
-        <PeriodSelectBtn>
-          <RightArrowIcon />
-        </PeriodSelectBtn>
-      </PeriodSelectWrapper>
-    </>
+      <PeriodSelectBtn
+        onClick={dateAdd}
+        className={isToday ? "isToday" : ""}
+        disabled={isToday ? true : false}
+      >
+        <RightArrowIcon />
+      </PeriodSelectBtn>
+    </PeriodSelectWrapper>
   );
 };
 
 export default PeriodSelector;
 
 const PeriodSelectBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
   background-color: #f6f6f6;
   border: 0px solid transparent;
-  width: 36px;
+  min-width: 36px;
   height: 36px;
   border-radius: 12px;
+  :hover {
+    background-color: #b3f0e8;
+  }
 
-  &.tomorrow {
+  &.isToday {
+    cursor: default;
+    :hover {
+      background-color: #f6f6f6;
+    }
   }
 `;
 
 const PeriodSelectWrapper = styled.div`
   margin: 15px 0px 49px 0px;
   color: #777;
-  /* display: flex;
+  display: flex;
   justify-content: space-between;
-  align-items: center; */
+  align-items: center;
+
+  .react-datepicker__input-container {
+    display: flex;
+    justify-content: center;
+  }
+  .react-datepicker {
+  }
+`;
+
+const PeriodPicker = styled(ReactDatePicker)`
+  border: 0px solid transparent;
+  text-align: center;
+  cursor: pointer;
+  caret-color: transparent;
+  ::placeholder {
+    color: red;
+  }
+  :focus {
+    outline: none;
+  }
 `;
