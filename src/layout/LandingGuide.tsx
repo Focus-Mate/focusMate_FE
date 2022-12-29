387a24fbb4cc6e66e415d4acb0a825eedee87571
김민경 <sparrowscout.0000@gmail.com>
2022-12-29 21:17:13 +0900

    [implement] periodselector API 요청 틀

--- src/layout/LandingGuide.tsx
+++ src/layout/LandingGuide.tsx
@@ -5,11 +5,13 @@ const LandingGuide = () => {
   const navigate = useNavigate();
   const isFirstEnter = useMatch("/");
 
+  //NOTE 추후에 login user / guest 분기점으로 사용
   useEffect(() => {
     if (isFirstEnter !== null) {
       navigate("/timer");
     }
   }, []);
+
   return <Outlet />;
 };
 
