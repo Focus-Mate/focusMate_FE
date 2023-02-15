e779bc33ccb40d9b13daaec89508091e0bb58f3a
reality023 <reality023@naver.com>
2023-02-15 22:21:01 +0900

    chore: theme 사용을 위한 ThemeProvider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { GlobalStyle } from "./style/globalStyle";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import ReactModal from "react-modal";
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme";
 
 // 모달 사용을 위한 최상위 컴포넌트 등록
 ReactModal.setAppElement("#root");
@@ -10,10 +12,12 @@ ReactModal.setAppElement("#root");
 const queryClient = new QueryClient();
 function App() {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<GlobalStyle />
-			<Router />
-		</QueryClientProvider>
+		<ThemeProvider theme={theme}>
+			<QueryClientProvider client={queryClient}>
+				<GlobalStyle />
+				<Router />
+			</QueryClientProvider>
+		</ThemeProvider>
 	);
 }
 
