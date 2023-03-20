26c6e71aa611aad770b40ce6b631af021615192a
김민경 <sparrowscout.0000@gmail.com>
2023-03-20 22:59:08 +0900

    add floatingModal

--- src/component/chart/BottomModal.tsx
+++ src/component/chart/BottomModal.tsx
@@ -1,12 +1,13 @@
 import instance from "@/axios";
 import { Title } from "@/style/globalStyle";
-import React, { Dispatch } from "react";
+import React, { Dispatch, useState } from "react";
 import ReactModal from "react-modal";
 import { useMutation, useQueryClient } from "react-query";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { ReactComponent as CloseIcon } from "@/style/icon/close.svg";
 import { AddIcon, DeleteIcon, EditIcon } from "@/style/icon/dday";
+import FloatingModal from "../FloatingModal";
 export interface SelectedDday {
   exam: string;
   dday: string;
@@ -18,37 +19,38 @@ interface ModalProps {
   setVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+export const customStyles = {
+  overlay: {
+    backgroundColor: "transparent",
+    zIndex: 10,
+    overflow: "hidden",
+  },
+  content: {
+    width: "100%",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "transparent",
+    height: "100%",
+    borderRadius: 0,
+    border: 0,
+    padding: 0,
+    overflow: "hidden",
+  },
+};
+
 export default function BottomModal({
   visible,
   setVisible,
   selecteExam,
 }: ModalProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
   const deleteDday = async (selectedDday: string) => {
     await instance.delete(`api/calculate/removeDay?exam=${selectedDday}`);
-  };
-
-  const customStyles = {
-    overlay: {
-      backgroundColor: "transparent",
-      zIndex: 10,
-      overflow: "hidden",
-    },
-    content: {
-      width: "100%",
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: "transparent",
-      height: "100%",
-      borderRadius: 0,
-      border: 0,
-      padding: 0,
-      overflow: "hidden",
-    },
   };
 
   const { mutateAsync: deleteDdayMutate } = useMutation(deleteDday, {
@@ -59,32 +61,61 @@ export default function BottomModal({
     },
   });
 
+  const onClickDeleteDday = () => {
+    setIsOpen(true);
+    setVisible(false);
+  };
+
   return (
-    <ReactModal isOpen={visible} style={customStyles}>
-      <BottomContainer>
-        <Content>
-          <ContentTitle>
-            D-DAY 설정 <CloseBtn onClick={() => setVisible(false)} />
-          </ContentTitle>
-          <SettingDday
-            onClick={() => deleteDdayMutate(selecteExam.exam)}
-            className="delete"
-          >
-            <DeleteIcon /> 삭제하기
-          </SettingDday>
-          <SettingDday onClick={() => navigate("/makedday")}>
-            <AddIcon /> 추가하기
-          </SettingDday>
-          <SettingDday
-            onClick={() =>
-              navigate(`/makedday/${selecteExam.exam}/${selecteExam.dday}`)
-            }
-          >
-            <EditIcon /> 수정하기
-          </SettingDday>
-        </Content>
-      </BottomContainer>
-    </ReactModal>
+    <>
+      <FloatingModal
+        isOpen={isOpen}
+        modalContent={{
+          comment: "D-DAY를 삭제할까요?",
+          options: [
+            {
+              id: 1,
+              guide: "아니요",
+              action: () => {
+                setVisible(true);
+                setIsOpen(false);
+              },
+            },
+            {
+              id: 2,
+              guide: "네, 삭제할게요",
+              action: () => {
+                deleteDdayMutate(selecteExam.exam);
+                setIsOpen(false);
+              },
+            },
+          ],
+        }}
+      />
+
+      <ReactModal isOpen={visible} style={customStyles}>
+        <BottomContainer>
+          <Content>
+            <ContentTitle>
+              D-DAY 설정 <CloseBtn onClick={() => setVisible(false)} />
+            </ContentTitle>
+            <SettingDday onClick={onClickDeleteDday} className="delete">
+              <DeleteIcon /> 삭제하기
+            </SettingDday>
+            <SettingDday onClick={() => navigate("/makedday")}>
+              <AddIcon /> 추가하기
+            </SettingDday>
+            <SettingDday
+              onClick={() =>
+                navigate(`/makedday/${selecteExam.exam}/${selecteExam.dday}`)
+              }
+            >
+              <EditIcon /> 수정하기
+            </SettingDday>
+          </Content>
+        </BottomContainer>
+      </ReactModal>
+    </>
   );
 }
 
