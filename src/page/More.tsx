a1ae8086ba597f72d5086d96b09c327d9089c200
reality023 <reality023@naver.com>
2023-03-26 19:55:04 +0900

    feat: SnackBar 제작 & .prettierrc.js 추가

--- src/page/More.tsx
+++ src/page/More.tsx
@@ -1,244 +1,261 @@
 // Library
-import { useState } from "react";
-import styled, { css } from "styled-components";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 // Components
-import MenuBox from "@/component/more/MenuBox";
+import MenuBox from '@/component/more/MenuBox';
 
 // Icons
-import iconNotice from "@/style/icon/more/notice.png";
-import iconDelete from "@/style/icon/more/delete.png";
-import iconFolder from "@/style/icon/more/folder.png";
-import iconMoon from "@/style/icon/more/moon.png";
-import iconOut from "@/style/icon/more/out.png";
-import iconPc from "@/style/icon/more/pc.png";
-import iconTerms from "@/style/icon/more/terms.png";
-import { useQuery } from "react-query";
-import instance from "@/axios";
-import { useRecoilState } from "recoil";
-import { isThemeDark } from "@/App";
-import ConfirmPop from "@/component/common/pop/ConfirmPop";
+import iconNotice from '@/style/icon/more/notice.png';
+import iconDelete from '@/style/icon/more/delete.png';
+import iconFolder from '@/style/icon/more/folder.png';
+import iconMoon from '@/style/icon/more/moon.png';
+import iconOut from '@/style/icon/more/out.png';
+import iconPc from '@/style/icon/more/pc.png';
+import iconTerms from '@/style/icon/more/terms.png';
+import { useQuery } from 'react-query';
+import instance from '@/axios';
+import { useRecoilState } from 'recoil';
+import { isThemeDark } from '@/App';
+import ConfirmPop from '@/component/common/pop/ConfirmPop';
+import { snackBarStatus } from '@/component/common/bar/StatusSnackBar';
 
 function Setting() {
-	const [darkMode, setDarkMode] = useRecoilState(isThemeDark);
-	const [isLogoutPop, setLogoutPop] = useState(false);
-	const navigate = useNavigate();
+  const [darkMode, setDarkMode] = useRecoilState(isThemeDark);
+  const [isLogoutPop, setLogoutPop] = useState(false);
+  const navigate = useNavigate();
 
-	const { data: response } = useQuery(["GetUser"], async () => {
-		const response = await instance.get("/api/user/me");
+  const { data: response } = useQuery(['GetUser'], async () => {
+    const response = await instance.get('/api/user/me');
 
-		return response;
-	});
+    return response;
+  });
 
-	return (
-		<Wrapper>
-			<ConfirmPop
-				options={{
-					message: "정말 로그아웃 하시나요?",
-					onCancel: () => {
-						setLogoutPop(false);
-					},
-					onConfirm: () => {
-						localStorage.removeItem("token");
-						navigate("/login");
-						setLogoutPop(false);
-					},
-					onCancelText: "아니요",
-					onConfirmText: "로그아웃하기",
-					isOpen: isLogoutPop,
-					setOpen: setLogoutPop,
-				}}
-			/>
-			<Container>
-				<Header>
-					<Title>설정</Title>
-					<User>
-						<PictureBox></PictureBox>
-						<Nickname>{response?.data?.nicknmae}</Nickname>
-						<ButtonArea onClick={() => navigate("/more/nick")}>
-							<Button>닉네임 수정</Button>
-						</ButtonArea>
-					</User>
-					<MenuBox
-						options={{
-							title: "이용 안내",
-						}}
-						items={[
-							<Item onClick={() => navigate("/more/notice")}>
-								<ItemIcon src={iconNotice} alt="notice" />
-								공지사항
-							</Item>,
-							<Item onClick={() => navigate("/more/service")}>
-								<ItemIcon src={iconTerms} alt="terms" />
-								서비스 이용약관
-							</Item>,
-							<Item onClick={() => navigate("/more/personal")}>
-								<ItemIcon src={iconTerms} alt="info" />
-								개인정보 처리방침
-							</Item>,
-						]}
-					/>
-					<MenuBox
-						options={{
-							title: "공지사항",
-						}}
-						items={[
-							<Item>
-								<ItemIcon src={iconMoon} alt="notice" />
-								<ItemText>다크모드 설정</ItemText>
-								<ItemContent>
-									<SwitchBox onClick={() => setDarkMode((mode) => !mode)} isDark={darkMode}>
-										<SwitchCircle isDark={darkMode} />
-									</SwitchBox>
-								</ItemContent>
-							</Item>,
-							<Item>
-								<ItemIcon src={iconPc} alt="notice" />
-								<ItemText>버전 정보</ItemText>
-								<ItemContent style={{ marginRight: "5px" }}>1.0.0</ItemContent>
-							</Item>,
-							<Item onClick={() => navigate("/more/license")}>
-								<ItemIcon src={iconFolder} alt="notice" />
-								<ItemText>오픈소스 라이선스</ItemText>
-							</Item>,
-							<Item
-								onClick={() => {
-									setLogoutPop(true);
-								}}
-							>
-								<ItemIcon src={iconOut} alt="notice" />
-								로그아웃
-							</Item>,
-							<Item>
-								<ItemIcon src={iconDelete} alt="notice" />
-								탈퇴하기
-							</Item>,
-						]}
-					/>
-				</Header>
-			</Container>
-		</Wrapper>
-	);
+  const [statusSnackBar, setStatusSnackBar] = useRecoilState(snackBarStatus);
+
+  return (
+    <Wrapper>
+      <ConfirmPop
+        options={{
+          message: '정말 로그아웃 하시나요?',
+          onCancel: () => {
+            setLogoutPop(false);
+          },
+          onConfirm: () => {
+            localStorage.removeItem('token');
+            navigate('/login');
+            setLogoutPop(false);
+          },
+          onCancelText: '아니요',
+          onConfirmText: '로그아웃하기',
+          isOpen: isLogoutPop,
+          setOpen: setLogoutPop,
+        }}
+      />
+      <Container>
+        <Header>
+          <Title>설정</Title>
+          <User>
+            <PictureBox></PictureBox>
+            <Nickname>{response?.data?.nicknmae}</Nickname>
+            <ButtonArea onClick={() => navigate('/more/nick')}>
+              <Button>닉네임 수정</Button>
+            </ButtonArea>
+          </User>
+          <MenuBox
+            options={{
+              title: '이용 안내',
+            }}
+            items={[
+              <Item onClick={() => navigate('/more/notice')}>
+                <ItemIcon src={iconNotice} alt="notice" />
+                공지사항
+              </Item>,
+              <Item onClick={() => navigate('/more/service')}>
+                <ItemIcon src={iconTerms} alt="terms" />
+                서비스 이용약관
+              </Item>,
+              <Item onClick={() => navigate('/more/personal')}>
+                <ItemIcon src={iconTerms} alt="info" />
+                개인정보 처리방침
+              </Item>,
+            ]}
+          />
+          <MenuBox
+            options={{
+              title: '공지사항',
+            }}
+            items={[
+              <Item>
+                <ItemIcon src={iconMoon} alt="notice" />
+                <ItemText>다크모드 설정</ItemText>
+                <ItemContent>
+                  <SwitchBox
+                    onClick={() => setDarkMode(mode => !mode)}
+                    isDark={darkMode}
+                  >
+                    <SwitchCircle isDark={darkMode} />
+                  </SwitchBox>
+                </ItemContent>
+              </Item>,
+              <Item>
+                <ItemIcon src={iconPc} alt="notice" />
+                <ItemText>버전 정보</ItemText>
+                <ItemContent style={{ marginRight: '5px' }}>1.0.0</ItemContent>
+              </Item>,
+              <Item onClick={() => navigate('/more/license')}>
+                <ItemIcon src={iconFolder} alt="notice" />
+                <ItemText>오픈소스 라이선스</ItemText>
+              </Item>,
+              <Item
+                onClick={() => {
+                  setLogoutPop(true);
+                }}
+              >
+                <ItemIcon src={iconOut} alt="notice" />
+                로그아웃
+              </Item>,
+              <Item>
+                <ItemIcon src={iconDelete} alt="notice" />
+                탈퇴하기
+              </Item>,
+              <button
+                onClick={() => {
+                  setStatusSnackBar({
+                    isOpen: true,
+                    timer: 2500,
+                    message: '공부시간을 저장했어요',
+                  });
+                }}
+              >
+                임시버튼
+              </button>,
+            ]}
+          />
+        </Header>
+      </Container>
+    </Wrapper>
+  );
 }
 export default Setting;
 
 const Wrapper = styled.div`
-	position: absolute;
-	left: 0;
-	top: 0;
-	width: 100%;
-	height: 100vh;
-	background-color: ${({ theme }) => theme.colors.bg.base};
-	padding: 20px;
-	color: ${({ theme }) => theme.colors.grey[800]};
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.bg.base};
+  padding: 20px;
+  color: ${({ theme }) => theme.colors.grey[800]};
 `;
 
 const Container = styled.div`
-	padding-bottom: 80px;
-	background-color: ${({ theme }) => theme.colors.bg.base};
+  padding-bottom: 80px;
+  background-color: ${({ theme }) => theme.colors.bg.base};
 `;
 
 const Header = styled.div`
-	padding-top: 2rem;
-	font-size: 1.25rem;
-	font-weight: 500;
+  padding-top: 2rem;
+  font-size: 1.25rem;
+  font-weight: 500;
 `;
 
 const Title = styled.h2``;
 
 const User = styled.div`
-	display: flex;
-	align-items: center;
-	margin-top: 1.5rem;
-	justify-content: space-between;
+  display: flex;
+  align-items: center;
+  margin-top: 1.5rem;
+  justify-content: space-between;
 `;
 
 const ButtonArea = styled.div`
-	padding: 8px;
-	flex-shrink: 0;
-	cursor: pointer;
+  padding: 8px;
+  flex-shrink: 0;
+  cursor: pointer;
 `;
 
 const Button = styled.button`
-	padding: 7px 8px;
-	border-radius: 50px;
-	border: 1px solid ${({ theme }) => theme.colors.grey[400]};
-	background-color: ${({ theme }) => theme.colors.bg.base};
-	color: ${({ theme }) => theme.colors.grey[600]};
-	cursor: pointer;
+  padding: 7px 8px;
+  border-radius: 50px;
+  border: 1px solid ${({ theme }) => theme.colors.grey[400]};
+  background-color: ${({ theme }) => theme.colors.bg.base};
+  color: ${({ theme }) => theme.colors.grey[600]};
+  cursor: pointer;
 `;
 
 const PictureBox = styled.div`
-	width: 4rem;
-	height: 4rem;
-	border-radius: 1.25rem;
-	background-color: ${({ theme }) => theme.colors.bg.mint10};
-	flex-shrink: 0;
+  width: 4rem;
+  height: 4rem;
+  border-radius: 1.25rem;
+  background-color: ${({ theme }) => theme.colors.bg.mint10};
+  flex-shrink: 0;
 `;
 
 const Nickname = styled.div`
-	margin-left: 0.5rem;
-	font-family: "SpoqaMedium";
-	width: 100%;
+  margin-left: 0.5rem;
+  font-family: 'SpoqaMedium';
+  width: 100%;
 `;
 
 const Item = styled.div`
-	display: flex;
-	align-items: center;
-	gap: 11px;
-	font-size: 1rem;
-	color: ${({ theme }) => theme.colors.grey[600]};
-	height: 30px;
-	width: 100%;
-	position: relative;
-	cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 11px;
+  font-size: 1rem;
+  color: ${({ theme }) => theme.colors.grey[600]};
+  height: 30px;
+  width: 100%;
+  position: relative;
+  cursor: pointer;
 `;
 
 const ItemIcon = styled.img`
-	width: 14px;
-	object-fit: contain;
-	padding-bottom: 2px;
+  width: 14px;
+  object-fit: contain;
+  padding-bottom: 2px;
 `;
 
 const ItemText = styled.div`
-	width: 100%;
+  width: 100%;
 `;
 const ItemContent = styled.div``;
 
 interface hasIsDark {
-	isDark: boolean;
+  isDark: boolean;
 }
 
 const SwitchBox = styled.div<hasIsDark>`
-	width: 56px;
-	height: 30px;
-	flex-shrink: 0;
-	background-color: ${({ theme }) => theme.colors.bg.line};
-	border-radius: 15px;
-	padding: 4px;
-	position: relative;
-	transition: background-color 0.3s;
+  width: 56px;
+  height: 30px;
+  flex-shrink: 0;
+  background-color: ${({ theme }) => theme.colors.bg.line};
+  border-radius: 15px;
+  padding: 4px;
+  position: relative;
+  transition: background-color 0.3s;
 
-	${({ isDark }) =>
-		isDark &&
-		css`
-			background-color: ${({ theme }) => theme.colors.bg.mint30};
-		`}
+  ${({ isDark }) =>
+    isDark &&
+    css`
+      background-color: ${({ theme }) => theme.colors.bg.mint30};
+    `}
 `;
 
 const SwitchCircle = styled.div<hasIsDark>(({ isDark }) => [
-	css`
-		width: 22px;
-		height: 22px;
-		border-radius: 50%;
-		background-color: ${({ theme }) => theme.colors.icon.white};
-		transition: left 0.3s;
-		position: absolute;
-		left: calc(0% + 4px);
-	`,
-	isDark &&
-		css`
-			left: calc(100% - 26px);
-		`,
+  css`
+    width: 22px;
+    height: 22px;
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.colors.icon.white};
+    transition: left 0.3s;
+    position: absolute;
+    left: calc(0% + 4px);
+  `,
+  isDark &&
+    css`
+      left: calc(100% - 26px);
+    `,
 ]);
--- src/page/Timer.tsx
+++ src/page/Timer.tsx
@@ -1,270 +1,298 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import styled from "styled-components";
+import { useCallback, useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
 
-import playIcon from "../style/icon/timer/play.png";
-import pauseIcon from "../style/icon/timer/pause.png";
-import stopIcon from "@/style/icon/timer/stop.png";
-import TimerSavePop from "@/component/timer/TimerSavePop";
-import TimerCircle from "@/component/timer/TimerCircle";
-import { useMutation } from "react-query";
-import instance from "@/axios";
+import playIcon from '../style/icon/timer/play.png';
+import pauseIcon from '../style/icon/timer/pause.png';
+import stopIcon from '@/style/icon/timer/stop.png';
+import TimerSavePop from '@/component/timer/TimerSavePop';
+import TimerCircle from '@/component/timer/TimerCircle';
+import { useMutation } from 'react-query';
+import instance from '@/axios';
 
 const TimerStatus = {
-	NONE: 0,
-	PLAYING: 1,
-	PAUSE: 2,
-	STOP: 3,
+  NONE: 0,
+  PLAYING: 1,
+  PAUSE: 2,
+  STOP: 3,
 };
 
 const circleWidth = 300;
 const radius = 135;
 
 function Timer() {
-	const [serverId, setServerId] = useState("");
+  const [serverId, setServerId] = useState('');
 
-	// React Query
-	const { mutateAsync: requestTimerStart } = useMutation("timer/start", async () => {
-		const response = await instance.post("/api/calculate/startTime");
+  useEffect(() => {
+    const api = async () => {
+      const response = await instance.get('/api/calculate/dayrecord', {
+        params: {
+          theDay: '2023-03-26',
+        },
+      });
+      console.log(response);
+    };
+    api();
+  });
 
-		return response.data;
-	});
+  // React Query
+  const { mutateAsync: requestTimerStart } = useMutation(
+    'timer/start',
+    async () => {
+      const response = await instance.post('/api/calculate/startTime');
 
-	const { mutateAsync: requestTimerStop } = useMutation("timer/stop", async (time: number) => {
-		console.log(serverId);
-		const response = await instance.put("/api/calculate/endTime", {
-			startTime: serverId,
-		});
-		return response.data;
-	});
+      console.log(response.data);
 
-	const dashArray = radius * Math.PI * 2;
-	const [dashOffset, setDashOffset] = useState(0);
+      return response.data;
+    },
+  );
 
-	const [playStatus, setPlayStatus] = useState(TimerStatus.NONE);
+  const { mutateAsync: requestTimerStop } = useMutation(
+    'timer/stop',
+    async (time: number) => {
+      console.log(serverId);
+      const response = await instance.put('/api/calculate/endTime', {
+        startTime: serverId,
+      });
+      return response.data;
+    },
+  );
 
-	// 시간을 멈추면 stackTime에 저장
-	const [nowTime, setNowTime] = useState(0);
-	const [stackTime, setStackTime] = useState(0);
+  const dashArray = radius * Math.PI * 2;
+  const [dashOffset, setDashOffset] = useState(0);
 
-	// 최종 결과 시간
-	const [resultTime, setResultTime] = useState(0);
+  const [playStatus, setPlayStatus] = useState(TimerStatus.NONE);
 
-	// 현재 기록중인 시간
-	const [milliSeconds, setMilliSeconds] = useState(0);
-	const [seconds, setSeconds] = useState(0);
-	const [minutes, setMinutes] = useState(0);
-	const [hours, setHours] = useState(0);
+  // 시간을 멈추면 stackTime에 저장
+  const [nowTime, setNowTime] = useState(0);
+  const [stackTime, setStackTime] = useState(0);
 
-	const startTime = useRef(0);
-	const endTime = useRef(0);
+  // 최종 결과 시간
+  const [resultTime, setResultTime] = useState(0);
 
-	const startTimer = useCallback(() => {
-		startTime.current = Date.now();
+  // 현재 기록중인 시간
+  const [milliSeconds, setMilliSeconds] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
 
-		function timeout() {
-			setTimeout(() => {
-				endTime.current = Date.now();
-				// 시간 정확도를 위해 setTimeout이 아닌 기기 시간을 사용해 계산
-				const time = new Date(endTime.current - startTime.current);
+  const startTime = useRef(0);
+  const endTime = useRef(0);
 
-				setHours(time.getUTCHours());
-				setMinutes(time.getUTCMinutes());
-				setSeconds(time.getUTCSeconds());
-				setMilliSeconds(time.getUTCMilliseconds());
+  const startTimer = useCallback(() => {
+    startTime.current = Date.now();
 
-				timeout();
-			}, 100);
-		}
+    function timeout() {
+      setTimeout(() => {
+        endTime.current = Date.now();
+        // 시간 정확도를 위해 setTimeout이 아닌 기기 시간을 사용해 계산
+        const time = new Date(endTime.current - startTime.current);
 
-		timeout();
-	}, []);
+        setHours(time.getUTCHours());
+        setMinutes(time.getUTCMinutes());
+        setSeconds(time.getUTCSeconds());
+        setMilliSeconds(time.getUTCMilliseconds());
 
-	// 전부 milliseconds로 변환하여 사용하기 위해 nowTime에 변환하여 저장
-	useEffect(() => {
-		const hourToMilliSeconds = hours * 3600000;
-		const minuteToMilliSeconds = minutes * 60000;
-		const secondToMilliSeconds = seconds * 1000;
-		setNowTime(hourToMilliSeconds + minuteToMilliSeconds + secondToMilliSeconds + milliSeconds);
-	}, [hours, minutes, seconds, milliSeconds]);
+        timeout();
+      }, 100);
+    }
 
-	// stackTime에 저장된 시간과 nowTime을 합하여 resultTime에 저장
-	useEffect(() => {
-		if (playStatus === TimerStatus.PLAYING) setResultTime(stackTime + nowTime);
-		if (playStatus === TimerStatus.PAUSE) {
-			setStackTime(resultTime);
-			setResultTime(resultTime);
-			setNowTime(0);
-			startTime.current = Date.now();
-		}
-		if (playStatus === TimerStatus.STOP) {
-			// 멈춘경우 로직
-		}
-	}, [playStatus, stackTime, nowTime, resultTime]);
+    timeout();
+  }, []);
 
-	useEffect(() => {
-		if (playStatus === TimerStatus.PLAYING) {
-			const timerPercentage = (100 / 60) * Math.floor((resultTime % 60000) / 1000);
-			setDashOffset(dashArray - (dashArray * timerPercentage) / 100); // 중간의 100이 퍼센테이지
-		}
-	}, [resultTime, dashArray, playStatus]);
+  console.log();
 
-	// 출력할 텍스트 생성
-	const allSeconds = Math.floor(resultTime / 1000);
-	const milliSecondsToHour = Math.floor(allSeconds / 3600);
-	const milliSecondsToMinute = Math.floor((allSeconds % 3600) / 60);
-	const milliSecondsToSecond = Math.floor(allSeconds % 60);
-	const hourText = milliSecondsToHour.toString().padStart(2, "0");
-	const minuteText = milliSecondsToMinute.toString().padStart(2, "0");
-	const secondText = milliSecondsToSecond.toString().padStart(2, "0");
+  // 전부 milliseconds로 변환하여 사용하기 위해 nowTime에 변환하여 저장
+  useEffect(() => {
+    const hourToMilliSeconds = hours * 3600000;
+    const minuteToMilliSeconds = minutes * 60000;
+    const secondToMilliSeconds = seconds * 1000;
+    setNowTime(
+      hourToMilliSeconds +
+        minuteToMilliSeconds +
+        secondToMilliSeconds +
+        milliSeconds,
+    );
+  }, [hours, minutes, seconds, milliSeconds]);
 
-	return (
-		<Container>
-			<TimerSavePop
-				playStatus={playStatus}
-				onResetClick={() => {
-					// setNowTime(0);
-					// setStackTime(0);
-					// setResultTime(0);
-					// setDashOffset(dashArray - (dashArray * 0) / 100);
-					setPlayStatus(TimerStatus.PAUSE);
-				}}
-				onConfirmClick={async () => {
-					// 저장 로직
-					const result = await requestTimerStop(Math.floor(resultTime / 1000));
-					setNowTime(0);
-					setStackTime(0);
-					setResultTime(0);
-					setDashOffset(dashArray - (dashArray * 0) / 100);
-					setPlayStatus(TimerStatus.NONE);
-					console.log(result);
-				}}
-			/>
-			<TimerCircle
-				circleWidth={circleWidth}
-				radius={radius}
-				dashArray={dashArray}
-				dashOffset={dashOffset}
-			>
-				<TimeText>공부시간 기록하기</TimeText>
-				<Time>
-					{hourText}:{minuteText}:{secondText}
-				</Time>
-			</TimerCircle>
-			<Controller>
-				<ButtonWrap>
-					{playStatus === TimerStatus.NONE && (
-						<button
-							onClick={async () => {
-								setPlayStatus(TimerStatus.PLAYING);
-								startTimer();
-								const result = await requestTimerStart();
+  // stackTime에 저장된 시간과 nowTime을 합하여 resultTime에 저장
+  useEffect(() => {
+    if (playStatus === TimerStatus.PLAYING) setResultTime(stackTime + nowTime);
+    if (playStatus === TimerStatus.PAUSE) {
+      setStackTime(resultTime);
+      setResultTime(resultTime);
+      setNowTime(0);
+      startTime.current = Date.now();
+    }
+    if (playStatus === TimerStatus.STOP) {
+      // 멈춘경우 로직
+    }
+  }, [playStatus, stackTime, nowTime, resultTime]);
 
-								setServerId(result.startTime);
-							}}
-						>
-							<img src={playIcon} alt="play" />
-						</button>
-					)}
-					{playStatus === TimerStatus.PLAYING && (
-						<button
-							onClick={() => {
-								setPlayStatus(TimerStatus.PAUSE);
-							}}
-						>
-							<img src={pauseIcon} alt="pause" />
-						</button>
-					)}
-					{playStatus === TimerStatus.PAUSE && (
-						<>
-							<button
-								onClick={() => {
-									setPlayStatus(TimerStatus.PLAYING);
-								}}
-							>
-								<img src={playIcon} alt="play" />
-							</button>
-							<button
-								onClick={async () => {
-									setPlayStatus(TimerStatus.STOP);
-								}}
-							>
-								<img src={stopIcon} alt="stop" />
-							</button>
-						</>
-					)}
-				</ButtonWrap>
-			</Controller>
-		</Container>
-	);
+  useEffect(() => {
+    if (playStatus === TimerStatus.PLAYING) {
+      const timerPercentage =
+        (100 / 60) * Math.floor((resultTime % 60000) / 1000);
+      setDashOffset(dashArray - (dashArray * timerPercentage) / 100); // 중간의 100이 퍼센테이지
+    }
+  }, [resultTime, dashArray, playStatus]);
+
+  // 출력할 텍스트 생성
+  const allSeconds = Math.floor(resultTime / 1000);
+  const milliSecondsToHour = Math.floor(allSeconds / 3600);
+  const milliSecondsToMinute = Math.floor((allSeconds % 3600) / 60);
+  const milliSecondsToSecond = Math.floor(allSeconds % 60);
+  const hourText = milliSecondsToHour.toString().padStart(2, '0');
+  const minuteText = milliSecondsToMinute.toString().padStart(2, '0');
+  const secondText = milliSecondsToSecond.toString().padStart(2, '0');
+
+  return (
+    <Container>
+      <TimerSavePop
+        playStatus={playStatus}
+        onResetClick={() => {
+          // setNowTime(0);
+          // setStackTime(0);
+          // setResultTime(0);
+          // setDashOffset(dashArray - (dashArray * 0) / 100);
+          setPlayStatus(TimerStatus.PAUSE);
+        }}
+        onConfirmClick={async () => {
+          // 저장 로직
+          const result = await requestTimerStop(Math.floor(resultTime / 1000));
+          setNowTime(0);
+          setStackTime(0);
+          setResultTime(0);
+          setDashOffset(dashArray - (dashArray * 0) / 100);
+          setPlayStatus(TimerStatus.NONE);
+          console.log(result);
+        }}
+      />
+      <TimerCircle
+        circleWidth={circleWidth}
+        radius={radius}
+        dashArray={dashArray}
+        dashOffset={dashOffset}
+      >
+        <TimeText>공부시간 기록하기</TimeText>
+        <Time>
+          {hourText}:{minuteText}:{secondText}
+        </Time>
+      </TimerCircle>
+      <Controller>
+        <ButtonWrap>
+          {playStatus === TimerStatus.NONE && (
+            <button
+              onClick={async () => {
+                setPlayStatus(TimerStatus.PLAYING);
+                startTimer();
+                const result = await requestTimerStart();
+
+                setServerId(result.startTime);
+              }}
+            >
+              <img src={playIcon} alt="play" />
+            </button>
+          )}
+          {playStatus === TimerStatus.PLAYING && (
+            <button
+              onClick={() => {
+                setPlayStatus(TimerStatus.PAUSE);
+              }}
+            >
+              <img src={pauseIcon} alt="pause" />
+            </button>
+          )}
+          {playStatus === TimerStatus.PAUSE && (
+            <>
+              <button
+                onClick={() => {
+                  setPlayStatus(TimerStatus.PLAYING);
+                }}
+              >
+                <img src={playIcon} alt="play" />
+              </button>
+              <button
+                onClick={async () => {
+                  setPlayStatus(TimerStatus.STOP);
+                }}
+              >
+                <img src={stopIcon} alt="stop" />
+              </button>
+            </>
+          )}
+        </ButtonWrap>
+      </Controller>
+    </Container>
+  );
 }
 
 export default Timer;
 
 const Container = styled.div`
-	position: absolute;
-	width: 100%;
-	height: 100vh;
-	padding: 0 1rem;
-	left: 0;
-	top: 0;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  padding: 0 1rem;
+  left: 0;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
-	svg {
-		margin-top: -120px;
-	}
+  svg {
+    margin-top: -120px;
+  }
 
-	@media (max-width: 330px) {
-		svg {
-			width: 100%;
-			height: auto;
-		}
-	}
+  @media (max-width: 330px) {
+    svg {
+      width: 100%;
+      height: auto;
+    }
+  }
 `;
 
 const TimeText = styled.div`
-	margin-bottom: 6px;
-	color: #2fc4bb;
+  margin-bottom: 6px;
+  color: #2fc4bb;
 `;
 
 const Time = styled.div`
-	font-size: 2.25rem;
-	font-weight: bold;
-	color: #018a93;
-	@media (max-width: 330px) {
-		font-size: 11vw;
-	}
+  font-size: 2.25rem;
+  font-weight: bold;
+  color: #018a93;
+  @media (max-width: 330px) {
+    font-size: 11vw;
+  }
 `;
 
 const Controller = styled.div`
-	position: relative;
-	width: 100%;
+  position: relative;
+  width: 100%;
 `;
 
 const ButtonWrap = styled.div`
-	position: absolute;
-	top: 2rem;
-	width: 100%;
-	display: flex;
-	justify-content: center;
-	gap: 1rem;
+  position: absolute;
+  top: 2rem;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
 
-	button {
-		width: 70px;
-		height: 70px;
-		background-color: #2fc4bb;
-		border: 0;
-		border-radius: 50%;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		cursor: pointer;
-		transition: background-color 0.3s;
+  button {
+    width: 70px;
+    height: 70px;
+    background-color: #2fc4bb;
+    border: 0;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: background-color 0.3s;
 
-		&:hover {
-			background-color: #3c9b95;
-		}
-	}
+    &:hover {
+      background-color: #3c9b95;
+    }
+  }
 `;
